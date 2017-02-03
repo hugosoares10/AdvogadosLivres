b435ab8c49710cb0270f51d5451b5eb531d6e949
Hugo <hugosoarespereira@gmail.com>
2017-02-02 23:55:39 -0200

    Teste
    Teste

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+				<h1 id = "message"><?php echo "AdvogadosLivres"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
